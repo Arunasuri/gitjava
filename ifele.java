e06d1b25a2730e04b6887e00947be5d281b231cf
Aruna <suri1aruna@gmail.com>
2024-10-13 13:43:45 -0400

    this is addup

---
+++ ifele.java
@@ -0,0 +1,10 @@
+package oct12th2024;
+
+public class ifele {
+    public static void main(String args[])
+    {
+        int a=25;
+        if (a>=10)
+            System.out.println("a is above 10");
+    }
+}
