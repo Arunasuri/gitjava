aed2513196cc88aada9c3e1a537d143e1b003198
Aruna <suri1aruna@gmail.com>
2024-10-13 13:57:17 -0400

    this is forloop

---
+++ forloop.java
@@ -0,0 +1,11 @@
+package oct12th2024;
+
+public class forloop {
+    public static void main(String args[])
+    {
+        for(int x=0;x<=5;x++)
+        {
+            System.out.println(x);
+        }
+    }
+}
